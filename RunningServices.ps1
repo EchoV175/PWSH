22ce0ed79b367bc5450fd0ff5e236147bce2311a
Hayden <77405787+EchoV175@users.noreply.github.com>
2021-04-13 15:23:28 -0500

    Add files via upload

---
+++ RunningServices.ps1
@@ -0,0 +1,3 @@
+# getting a all running services 
+Get-Service | Where-Object {$_.Status -eq 'Running'} 
+<# Get-Service fetches all services. "|" passes the output of Get-Service into the next command. Where-Object filters the results as specified in the {}. "$_.Status" is a variable for the current object in the pipeline, and its property of "Status". "-eq" is a comparison operator that means equal to. Finally, 'Running' is the filter specified by the comparison operator "-eq". #>
